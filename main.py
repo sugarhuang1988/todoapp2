3b730aa243d12b8e25485e1bd389a328159468d0
sugarhuang1988 <15593364+sugarhuang1988@user.noreply.gitee.com>
2025-03-19 16:57:15 +0800

    main info change

--- main.py
+++ main.py
@@ -31,5 +31,5 @@ app.include_router(auth.router)
 app.include_router(users.router)
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8090)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8090)
